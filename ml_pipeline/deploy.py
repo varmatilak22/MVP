40ff6ea14de5c78689339d2455fc8daad539eb47
varmatilak22 <varmatilak22@gmail.com>
2024-07-24 04:17:21 +0530

    Making Absolute path from relative path in linear_regression.pkl file

--- ml_pipeline/deploy.py
+++ ml_pipeline/deploy.py
@@ -1,11 +1,22 @@
 import streamlit as st
 import joblib
 import numpy as np
+import os
 
 def load_model(model_path):
     return joblib.load(model_path)
 
-model=load_model('../models/linear_regression.pkl')
+deploy_dir=os.path.dirname(__file__)
+
+parent_dir=os.path.dirname(deploy_dir)
+
+models_dir=os.path.join(parent_dir,'models')
+
+model_path=os.path.join(models_dir,'linear_regression.pkl')
+
+
+print(model_path)
+model=load_model(model_path)
 st.title("Simple Linear Regression Model")
 
 input_value=st.number_input("Enter a Value for Prediction:",min_value=0)
