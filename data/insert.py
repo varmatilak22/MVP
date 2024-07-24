195cb16f7486f810fe8f19aacb642b62de33bfb1
varmatilak22 <varmatilak22@gmail.com>
2024-07-24 19:30:44 +0530

    Adding more Data in linear.db random number with linear relationship to output variable

--- data/insert.py
+++ data/insert.py
@@ -1,10 +1,11 @@
 import sqlite3
+import random 
 
 conn=sqlite3.connect("linear.db")
 cursor=conn.cursor()
 
 #Insert 
-
+'''
 cursor.execute("""
 insert into data values 
 (1,2),
@@ -18,7 +19,15 @@ insert into data values
 (9,18),
 (10,20)
 """)
+'''
+random_numbers=[random.randint(0,1000) for _ in range(1000) ]
+print(len(random_numbers))
+x=[num for num in random_numbers]
+y=[(2*num)+3 for num in random_numbers]
 
+print(x,y)
+data_to_insert=list(zip(x,y))
+cursor.executemany("insert into data(X,y) values (?,?)",data_to_insert)
 
 cursor.execute("select * from data;")
 data=cursor.fetchall()
